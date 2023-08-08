ef4f75dc7db31306149537fc8fc059512b5d1849
Elshan Namazov <103148752+elshanamazov@users.noreply.github.com>
2023-08-08 12:56:51 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -41,4 +41,13 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   })();
+
+  const currentPath = window.location.pathname.substring(1);
+    const navLinks = document.querySelectorAll(".nav__link");
+
+    navLinks.forEach((link) => {
+      if (link.getAttribute("href") === currentPath) {
+        link.classList.add("_active");
+      }
+    });
 });
