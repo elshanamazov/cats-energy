90f225935677f623b09681aabc7993078e893f85
Elshan Namazov <103148752+elshanamazov@users.noreply.github.com>
2023-08-08 13:12:15 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -32,7 +32,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // active link
   (() => {
-    const currentPath = window.location.pathname.replace("cats-energy/", "");
+    const currentPath = window.location.pathname;
+    currentPath.replace("cats-energy/", "");
     console.log(currentPath);
     const navLinks = document.querySelectorAll(".nav__link");
 
